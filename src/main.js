8421536840e38df0b5cbcf8dc9fcce7c8f503b43
Sekai <maximruchko4@gmail.com>
2023-09-18 21:04:48 +0900

    добавил мета тэг тест

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-createApp(App).use(router).mount('#app')
+import VueMeta from 'vue-meta';
+createApp(App).use(router).use(VueMeta).mount('#app')
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -10,6 +10,16 @@ import HelloWorld from '@/components/HomePage.vue'
 
 export default {
   name: 'HomeView',
+  metaInfo: {
+    title: 'Всё о методах шифрования', // Заголовок страницы
+    meta: [
+      {
+        name: 'description',
+        content: 'На этом сайте вы сможете ознакомится с различными методами шифрования информации и погрузится в мир криптографии' // Мета-описание
+      },
+
+    ]
+  },
   components: {
     HelloWorld
   }
